3ca1edbe5838b2d05a8041c5300be2214ec84ed8
saegeulu <saegeulu@naver.com>
2023-06-09 17:14:29 +0900

    문제3 해결

---
+++ if_statement_p3.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(){
+    int sum=0;
+    for(int i=0;i<=1000;i++){
+        if(i%3==0||i%5==0){
+            sum = sum + i;
+        }
+    }
+    printf("1000 이하의 3의 배수 또는 5의 배수인 자연수들의 합은 %d 입니다.\n", sum);
+}

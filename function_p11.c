cefc3c6b26b35ad3ae36ddffebaceca522779636
saegeulu <saegeulu@naver.com>
2023-06-10 21:41:04 +0900

    함수 문제 11 ing

---
+++ function_p11.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+//N 진법에서 M 진법으로 변환하는 프로그램을 만들어보세요. 
+
+
+
+int main(){
+    int N;
+    printf("Set neumeral system");
+    scanf("%d",&N);
+    //N 진법에서 10 진법으로
+    
+    //10 진법에서 M 진법으로
+
+}

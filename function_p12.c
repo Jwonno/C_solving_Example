7274d7ddd53cc57dea8793e74eee71d029eb0bc3
saegeulu <saegeulu@naver.com>
2023-06-13 13:46:44 +0900

    함수 문제 12 일시중단

--- function_p12.c
+++ function_p12.c
@@ -4,15 +4,17 @@
 물론, 소수 부도 1000 자리로 구현해도 됩니다. 
 1000 자리 수들의 연산 수행 시간은 1 초 미만이여야합니다.*/
 
+//덧셈 함수
 void adder(char Num1[], char Num2[],char sum[],int N){
     int carry = 0;
-    for(int i=0;i<N;i++){
+    for(int i=0;i<N-1;i++){
             sum[i]=Num1[i]+Num2[i]+carry;
-            if(sum[i]>=10&&i!=N-1){
+            if(sum[i]>=10){
                 carry=1;
                 sum[i]-=10;
             }
     }
+    sum[N-1]=Num1[N-1]+Num2[N-1]+carry;
     //결과 출력
     printf("[Sum]\n");
     for(int i=N-1;i>=0;i--){
@@ -20,7 +22,7 @@ void adder(char Num1[], char Num2[],char sum[],int N){
     }
     printf("\n\n");
 }
-
+//뺄셈 함수
 void subtractor(char Num1[], char Num2[], char sub[], int N){
     int borrow = 0;
     for(int i=0;i<N;i++){
@@ -41,7 +43,22 @@ void subtractor(char Num1[], char Num2[], char sub[], int N){
     }
     printf("\n\n");
 }
-
+//곱셈 함수
+/*void multiplication(char Num1[], char Num2[], char mul[], int N){
+    int i=0;
+        for(int i=0,j=0;j<N;j++,i++){
+            for(int k=1;k<N;k++,i++){
+                mul[i]=(Num1[k]*Num2[i]+(Num1[k-1]*Num1[i])/10)%10;
+            }
+            i--;
+        }
+    //결과 출력
+    printf("[Multiplication]\n");
+    for(int i=N*N-1;i>=0;i--){
+        printf("%c",mul[i]+48);
+    }
+    printf("\n\n");
+}*/
 int main(){
     
     int place_N;
@@ -49,15 +66,16 @@ int main(){
     char Num2[1000]={0};
     char sum[1000]={0};
     char sub[1000]={0};
+    char mul[1000]={0};
     printf("Enter the place number(MAX_1000): ");
     scanf("%d",&place_N);
-    printf("\nEnter the NUM_1(낮은 자리수부터 입력): ");
-    for(int i=0;i<place_N;i++){
+    printf("\nEnter the NUM_1(높은 자리수부터 입력): ");
+    for(int i=place_N-1;i>=0;i--){
         scanf(" %c",&Num1[i]);
         Num1[i]-=48;
     }
-    printf("\nEnter the NUM_2(낮은 자리수부터 입력): ");
-    for(int i=0;i<place_N;i++){
+    printf("\nEnter the NUM_2(높은 자리수부터 입력): ");
+    for(int i=place_N-1;i>=0;i--){
         scanf(" %c",&Num2[i]);
         Num2[i]-=48;
     }
@@ -67,6 +85,9 @@ int main(){
     
     //뺄셈 Num1-Num2
     subtractor(Num1, Num2, sub,place_N);
+
+    //곱셈 Num1*Num2
+    //multiplication(Num1, Num2, mul, place_N);
 
 return 0;
 }

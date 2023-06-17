fb88bff996db9f6c48437c6beca37446082d82de
saegeulu <saegeulu@naver.com>
2023-06-17 20:51:42 +0900

    도서관리프로그램 ing

--- string_p6.c
+++ string_p6.c
@@ -13,14 +13,14 @@ void add(int *borrowed, int *num_total_book, char book_title[][30], char book_au
 void search(int *borrowed, int num_total_book, char book_title[][30], char book_auth[][30], char book_publ[][30]);
 
 //책을 빌리는 기능
-void borrow();
+void borrow(int *borrowed);
 
 //책을 반납하는 기능
 void re_turn();
 
 int main(){
     printf("This is library program.\n");
-    int choice = 1;
+    int choice=1;
     int num_total_book = 0;
 
     char book_title[100][30], book_auth[100][30], book_publ[100][30];
@@ -41,12 +41,12 @@ int main(){
             
             case 2:
             printf("Search\n");
-            //search();
+            search(borrowed, num_total_book, book_title, book_auth, book_publ);
             break;
             
             case 3:
             printf("Borrow books\n");
-            //borrow();
+            borrow(borrowed);
             break;
             
             case 4:
@@ -63,7 +63,6 @@ int main(){
             printf("choose again\n");
         }
     }
-
     return 0;
 }
 
@@ -76,33 +75,89 @@ void add(int *borrowed, int *num_total_book, char book_title[][30], char book_au
         scanf("%s",book_auth[*num_total_book]);
         printf("Enter the publisher of the book: ");
         scanf("%s",book_publ[*num_total_book]);
-        borrowed[*num_total_book] = 1;
-        *num_total_book++;
+        borrowed[*num_total_book] = 0;
+        (*num_total_book)++;
         printf("Success to add a new book\n\n");
     }
 }
 
+int compare(char *str, char *word){
+   int j=0;
+   for(int i=0;str[i];i++){
+    for(;word[j];j++){
+        if(str[i+j]!=word[j]){
+            j=0;
+            break;
+        }
+    }
+    if(j!=0) return 1;
+   }
+   return 0;
+}
+
 void search(int *borrowed, int num_total_book, char book_title[][30], char book_auth[][30], char book_publ[][30]){
     char word[30];
-    printf("Enter the search word: ");
-    scanf("%s",word);
-    for(int i=0;i<num_total_book;i++){
-       
-        int k=0;
-        for(int j=0;book_title[i][j];j++){
-            for(;word[k];k++){
-                if(book_title[i][j+k]!=word[k]){
-                k=0;
-                break;
-                }
-            }
-            if(j!=0) {
-                printf("title: \"%s\"\n",book_title[i]);
-                printf("author: \"%s\"\n",book_auth[i]);
-                printf("publisher: \"%s\"\n",book_publ[i]);
+    int user_choice;
+    printf("1. Search title \n");
+    printf("2. Search author \n");
+    printf("3. Search publisher \n");
+    printf("others: back to menu\n");
+    scanf("%d",&user_choice);
+
+    //제목 검색
+    if(user_choice==1){
+        printf("Enter the search word: ");
+        scanf("%s",word);
+        for(int i=0;i<num_total_book;i++){
+            if(compare(book_title[i],word)==1){
+                printf("Number: \"%d\"\n",i+1);
+                printf("Title: \"%s\"\n",book_title[i]);
+                printf("Author: \"%s\"\n",book_auth[i]);
+                printf("Publisher: \"%s\"\n",book_publ[i]);
+                if(borrowed[i]==1) printf("Borrowed\n");
+                else printf("Can borrow\n");
             }
         }
-        printf("Not exist\n\n");
     }
+    //작가 검색
+    else if(user_choice==2){
+        printf("Enter the search word: ");
+        scanf("%s",word);
+        for(int i=0;i<num_total_book;i++){
+            if(compare(book_auth[i],word)==1){
+                printf("Number: \"%d\"\n",i+1);
+                printf("Title: \"%s\"\n",book_title[i]);
+                printf("Author: \"%s\"\n",book_auth[i]);
+                printf("Publisher: \"%s\"\n",book_publ[i]);
+                if(borrowed[i]==1) printf("Borrowed\n");
+                else printf("Can borrow\n");
+            }
+        }
+    }
+    //출판사 검색
+    else if(user_choice==3){
+        printf("Enter the search word: ");
+        scanf("%s",word);
+        for(int i=0;i<num_total_book;i++){
+            if(compare(book_publ[i],word)==1){
+                printf("Number: \"%d\"\n",i+1);
+                printf("Title: \"%s\"\n",book_title[i]);
+                printf("Author: \"%s\"\n",book_auth[i]);
+                printf("Publisher: \"%s\"\n",book_publ[i]);
+                if(borrowed[i]==1) printf("Borrowed\n");
+                else printf("Can borrow\n");
+            }
+        }
+    }
+}
 
+void borrow(int*borrowed){
+    int num;
+    printf("Enter the book number: ");
+    scanf("%d",&num);
+    if(borrowed[num-1]==1) printf("Already borrowed\n");
+    else {
+        borrowed[num-1]=1;
+        printf("Success to borrow\n");
+    }
 }

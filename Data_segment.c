0eee920d23d04fc98aec3665fafa31e0de63d87d
saegeulu <saegeulu@naver.com>
2023-06-18 20:08:02 +0900

    데이터 세크먼트 내용

--- Data_segment.c
+++ Data_segment.c
@@ -1,15 +1,15 @@
 /*메모리의 배치 모습*/
 
 #include <stdio.h>
-int global = 3;
+int global = 3; 
 int main(){
     
     char *str="Hello, Baby";
-    int i;
-    char arr[20] = "WHATTHEHECK";
+    int i; //stack에 저장
+    char arr[20] = "WHATTHEHECK"; //stack에 저장
 
     printf("global: %p\n",&global);
     printf("i: %p\n", &i);
     printf("str: %p\n",str);
-    printf("arr: %p\n",arr);
+    printf("arr: %p\n",arr); //i 보다 나중에 추가 된 arr 의 주소값이 더 작다.
 }

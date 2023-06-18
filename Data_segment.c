2c73fee22094661a05602c2ae378328771f24932
saegeulu <saegeulu@naver.com>
2023-06-18 20:07:34 +0900

    데이터 세크먼트 내용

---
+++ Data_segment.c
@@ -0,0 +1,15 @@
+/*메모리의 배치 모습*/
+
+#include <stdio.h>
+int global = 3;
+int main(){
+    
+    char *str="Hello, Baby";
+    int i;
+    char arr[20] = "WHATTHEHECK";
+
+    printf("global: %p\n",&global);
+    printf("i: %p\n", &i);
+    printf("str: %p\n",str);
+    printf("arr: %p\n",arr);
+}

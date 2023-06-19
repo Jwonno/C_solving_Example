dc96737129fa6765f74a83020c8d924d850c5d2c
saegeulu <saegeulu@naver.com>
2023-06-19 20:56:00 +0900

    메인 함수 인자 문제ing

--- main_calc.c
+++ main_calc.c
@@ -3,41 +3,41 @@
 15 가 나오게 하면 되지요.*/
 #include <stdio.h>
 #include <stdlib.h>
-
+int get_int(char *num);
 int main(int argc, char **argv){
-    int digit=0, num1=0, num2=0, sum, sub, mul, div;
+    int digit=1, num1=0, num2=0;
     //num1 구하기
-    for(int i=0;argv[1][i];i++){
-        digit++;
+    for(int i=1;argv[1][i];i++){
+        digit*=10;
     }
-    digit--;
-    if(digit==0) num1=*argv[1]-48;
-    while(digit+1){
-        int temp=1;
-        for(int i=digit;i>0;i--){
-            temp*=10;
-        }
-        num1+=temp*(argv[1][digit]-48);
-        digit--;
+    for(int index=0;argv[1][index];index++){
+        num1+=digit*(argv[1][index]-48);
+        digit/=10;
     }
-    digit = 0;
+    digit = 1;
+
     //num2 구하기
-    for(int i=0;argv[1][i];i++){
-        digit++;
+    for(int i=1;argv[3][i];i++){
+        digit*=10;
     }
-    digit--;
-    if(digit==0) num2=*argv[3]-48;
-    while(digit+1){
-        int temp=1;
-        for(int i=digit;i>0;i--){
-            temp*=10;
-        }
-        num2+=temp*(argv[3][digit]-48);
-        digit--;
+    for(int index=0;argv[3][index];index++){
+        num2+=digit*(argv[3][index]-48);
+        digit/=10;
     }
-    sum = num1+num2;
-    sub = num1-num2;
-    mul = num1*num2;
-    div = num1/num2;
-    printf("계산 결과는 %d 이다.\n",sum);
+    
+    if(argv[2][0]=='+'){
+    printf("계산결과는 %d 이다.\n",num1+num2);
+    }
+    else if(argv[2][0]=='-'){
+    printf("계산결과는 %d 이다.\n",num1-num2);
+    }
+    else if(argv[2][0]==42){
+    printf("계산결과는 %d 이다.\n",num1*num2);
+    }
+    else if(argv[2][0]=='/'){
+    printf("계산결과는 %d 이다.\n",num1/num2);
+    }
+    else printf("error");
+
+    return 0;
 }

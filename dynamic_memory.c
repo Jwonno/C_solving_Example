f23b79dd09e5c61d537240b1f6f939f917cd28fc
saegeulu <saegeulu@naver.com>
2023-06-20 17:34:05 +0900

    동적 할당 내용

--- dynamic_memory.c
+++ dynamic_memory.c
@@ -58,7 +58,7 @@ int main(){
     return 0;
 }*/
 
-//포인터 배열을 이용한 2차원 배열 할당하기의 활용
+/*//포인터 배열을 이용한 2차원 배열 할당하기의 활용
 int main(int argc, char **argv){
     int sum = 0;
     int subject, students;
@@ -94,4 +94,80 @@ int main(int argc, char **argv){
         free(arr[i]);
     }
     free(arr);
+}*/
+
+/*//진짜 2차원 배열 할당하기(컴파일 오류 발생 시 gcc로 컴파일 할 것)
+//int arr[heitgh][width] 와 같이 생긴 2차원 배열을 할당하고자 한다.
+//이 메모리의 크기는 height*width*sizeof(int) 가 될 것이다.
+
+void print_array(int width, int height, int (*arr)[width]);
+//인자의 순서가 (int(*arr)[width], int width, int height) 순서라면 width 선언이 나중에 있으므로 컴파일 오류가 발생한다.
+//따라서 인자의 순서를 (int width, int(*arr)[width], int height) 와 같이 width 선언을 미리 해줘야한다.
+int main(){
+    int height, width;
+    printf("행의 너비: ");
+    scanf("%d",&width);
+    printf("열의 높이: ");
+    scanf("%d",&height);
+
+    int (*arr)[width] = (int(*)[width])malloc(height*width*sizeof(int)); 
+    //포인터 타입에 행의 길이가 들어가야하므로 (int*) 가 아닌 (int(*)[width]) 를 사용한다.
+    for(int i=0;i<height;i++){
+        for(int j=0;j<width;j++){
+            scanf("%d",&arr[i][j]);
+        }
+    }
+    print_array(width, height, arr);
+
+    free(arr);
+
+    return 0;
 }
+
+void print_array(int width, int height, int (*arr)[width]){
+    for(int i=0;i<height;i++){
+        for(int j=0;j<width;j++){
+            printf("%d ",arr[i][j]);
+        }
+        printf("\n");
+    }
+}*/
+
+/*//진짜 2차원 배열 할당하기의 활용(컴파일 오류 발생 시 gcc로 컴파일 할 것)
+void get_average(int num_student, int num_subject, int (*scores)[num_student]);
+
+int main(){
+    int subject, students;
+
+    printf("과목 수: ");
+    scanf("%d",&subject);
+
+    printf("학생 수: ");
+    scanf("%d",&students);
+
+    int(*scores)[students];
+    scores = (int(*)[students])malloc(sizeof(int)*students*subject);
+
+    for(int i=0;i<subject;i++){
+        printf("Subject_%d--------------\n",i+1);
+        for(int j=0;j<students;j++){
+            printf("Student_%d: ",j+1);
+            scanf("%d",&scores[i][j]);
+        }
+    }
+    get_average(students, subject, scores);
+    free(scores);
+
+    return 0;
+}
+
+void get_average(int num_student, int num_subject, int (*scores)[num_student]){
+    for(int i=0;i<num_subject;i++){
+        int sum =0;
+        for(int j=0;j<num_student;j++){
+            sum+=scores[i][j];
+        }
+        printf("Average of Subject_%d: %d\n",i+1,sum/num_student);
+    }
+}
+*/

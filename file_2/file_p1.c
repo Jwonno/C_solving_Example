d0ec2ba2cd3a903e709455aafbb11cef27bfa6d9
saegeulu <saegeulu@naver.com>
2023-06-30 16:32:48 +0900

    파일과 연결하는 도서관리 프로그램 및 문제 1 해결

--- file_2/file_p1.c
+++ file_2/file_p1.c
@@ -22,7 +22,9 @@ AAA
 와 같은 형식 이지요. 물론 파일에 도서 목록을 출력하는 작업도 수정해야 합니다. */
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
+enum{N, Y};
 typedef struct BOOK {
     char title[30];
     char auth[30];
@@ -40,21 +42,27 @@ void add(int *num_total_book, BOOK *book);
 void search(int num_total_book, BOOK *book);
 
 //책을 빌리는 기능
-void borrow(BOOK *book);
+void borrow(BOOK *book, int num_total_book);
 
 //책을 반납하는 기능
-void re_turn(BOOK *book);
+void re_turn(BOOK *book, int num_total_book);
+
+//책의 정보를 출력하는 기능 
+void print_book(BOOK book);
 
 //책 목록을 파일에 출력하는 기능
-int print_book_list(BOOK *book, int num_total_bok);
+int print_book_list(BOOK *book, int num_total_book);
+
+//파일로부터 도서목록을 불러오는 기능
+int load_book_list(BOOK *book, int *num_total_book);
+
+//문자열을 정수로 변환시키는 함수
+int get_int(char*num);
 
 int main(){
-
     printf("This is library program.\n");
-    int choice=1;
+    int choice;
     int num_total_book = 0;
-
-    BOOK book[100];
 
     printf("Please set the maximum number of books that can be stored: ");
     scanf("%d",&choice);
@@ -66,7 +74,9 @@ int main(){
         printf("Choice[2]: Search\n");
         printf("Choice[3]: Borrow books\n");
         printf("Choice[4]: Return books\n");
-        printf("Choice[5]: End program\n");
+        printf("Choice[5]: Print to file book_list.txt\n");
+        printf("Choice[6]: Road to file book_list.txt\n");
+        printf("Choice[7]: End program\n");
         scanf("%d",&choice);
         switch(choice){
             case 1:
@@ -81,24 +91,32 @@ int main(){
             
             case 3:
             printf("Borrow books\n");
-            borrow(book);
+            borrow(book, num_total_book);
             break;
             
             case 4:
             printf("Return books\n");
-            re_turn(book);
+            re_turn(book, num_total_book);
             break;
             
             case 5:
+            print_book_list(book, num_total_book);
+            break;
+
+            case 6:
+            load_book_list(book, &num_total_book);
+            break;
+
+            case 7:
             printf("Thank you");
-            choice = 0;
             break;
 
             default:
             printf("choose again\n");
         }
-        if(choice==5) break;
+        if(choice==7) {break;}
     }
+    free(book);
     return 0;
 }
 
@@ -112,7 +130,7 @@ void add(int *num_total_book, BOOK *book){
         scanf("%s",book[*num_total_book].auth);
         printf("Enter the publisher of the book: ");
         scanf("%s",book[*num_total_book].publ);
-        book[*num_total_book].borrowed = 0;
+        book[*num_total_book].borrowed = N;
         (*num_total_book)++;
         printf("Success to add a new book\n\n");
     }
@@ -134,7 +152,7 @@ int compare(char *str, char *word){
 }
 
 //책 검색 기능
-void add(int num_total_book, BOOK *book){
+void search(int num_total_book, BOOK *book){
     char word[30];
     int user_choice;
     printf("1. Search title \n");
@@ -148,12 +166,7 @@ void add(int num_total_book, BOOK *book){
         scanf("%s",word);
         for(int i=0;i<num_total_book;i++){
             if(compare(book[i].title,word)==1){
-                printf("Number: \"%d\"\n",i+1);
-                printf("Title: \"%s\"\n",book[i].title);
-                printf("Author: \"%s\"\n",book[i].auth);
-                printf("Publisher: \"%s\"\n",book[i].publ);
-               if(book[i].borrowed==1) printf("Borrowed\n\n");
-                else printf("Can borrow\n\n");
+                print_book(book[i]);
             }
         }
     }
@@ -163,12 +176,7 @@ void add(int num_total_book, BOOK *book){
         scanf("%s",word);
         for(int i=0;i<num_total_book;i++){
            if(compare(book[i].auth,word)==1){
-                printf("Number: \"%d\"\n",i+1);
-                printf("Title: \"%s\"\n",book[i].title);
-                printf("Author: \"%s\"\n",book[i].auth);
-                printf("Publisher: \"%s\"\n",book[i].publ);
-               if(book[i].borrowed==1) printf("Borrowed\n\n");
-                else printf("Can borrow\n\n");
+                print_book(book[i]);
             }
         }
     }
@@ -178,36 +186,122 @@ void add(int num_total_book, BOOK *book){
         scanf("%s",word);
         for(int i=0;i<num_total_book;i++){
            if(compare(book[i].publ,word)==1){
-                printf("Number: \"%d\"\n",i+1);
-                printf("Title: \"%s\"\n",book[i].title);
-                printf("Author: \"%s\"\n",book[i].auth);
-                printf("Publisher: \"%s\"\n",book[i].publ);
-               if(book[i].borrowed==1) printf("Borrowed\n\n");
-                else printf("Can borrow\n\n");
+                print_book(book[i]);
             }
         }
     }
 }
 
+//책의 정보를 출력하는 기능
+void print_book(BOOK book){
+    printf("Title: \"%s\"\n",book.title);
+    printf("Author: \"%s\"\n",book.auth);
+    printf("Publisher: \"%s\"\n",book.publ);
+    if(book.borrowed==Y) printf("Borrowed\n\n");
+    else if(book.borrowed==N) printf("Can borrow\n\n");
+}
+
 //책 대출 기능
-void borrow(BOOK *book){
+void borrow(BOOK *book, int num_total_book){
     int num;
     printf("Enter the book number: ");
     scanf("%d",&num);
-    if(book[num-1].borrowed==1) printf("Already borrowed\n");
-    else {
-        book[num-1].borrowed=1;
-        printf("Success to borrow\n\n");
+    if(0<num<=num_total_book) {
+        if(book[num-1].borrowed==Y) printf("Already borrowed\n\n");
+        else if(book[num-1].borrowed==N){
+            book[num-1].borrowed=Y;
+            printf("Success to borrow\n\n");
+        }
     }
+    else printf("Not Exists\n\n");
 }
 //책 반납 기능
-void re_turn(BOOK *book){
+void re_turn(BOOK *book, int num_total_book){
     int num;
     printf("Enter the book number: ");
     scanf("%d",&num);
-    if(book[num-1].borrowed==0) printf("not borrowed\n");
-    else {
-        book[num-1].borrowed=0;
-        printf("Success to return\n\n");
+    if(0<num<=num_total_book) {
+        if(book[num-1].borrowed==N) printf("not borrowed\n\n");
+        else if(book[num-1].borrowed==Y) {
+            book[num-1].borrowed=N;
+            printf("Success to return\n\n");
+        }
     }
+    else printf("Not Exists\n\n");
+}
+
+//파일에 도서목록을 출력하는 기능
+int print_book_list(BOOK *book, int num_total_book){
+    FILE*fp = fopen("C:\\Users\\dnjsg\\source\\C_language_training\\file_2\\book_list.txt","w");       
+    if(fp==NULL){
+        printf("WRITE ERROR!!!\n");
+        return 0;
+    }
+
+    fprintf(fp, "%d\n",num_total_book);
+    
+    printf("Title\nAuthor\nPublisher\n");
+
+    for(int i=0;i<num_total_book;i++){
+        fprintf(fp, "%s\n",book[i].title);
+        fprintf(fp, "%s\n",book[i].auth);
+        fprintf(fp, "%s\n",book[i].publ);
+        if(book[i].borrowed==Y) fprintf(fp, "Y\n");
+        else fprintf(fp, "N\n");
+    }
+    fclose(fp);
+    printf("Success to print book list to book_list.txt\n");
+    return 0;
+}
+
+//파일로부터 도서목록을 불러오는 기능
+int load_book_list(BOOK *book, int *num_total_book){
+    
+    char num[10];
+    char buf[2];
+
+    FILE*fp = fopen("C:\\Users\\dnjsg\\source\\C_language_training\\file_2\\book_list.txt","r");
+    if(fp==NULL){
+        printf("READ ERROR!!!\n");
+        return 0;
+    }
+
+    fgets(num,9,fp);
+    *num_total_book = get_int(num);
+
+    for(int i=0;i<(*num_total_book);i++){
+        int condition = 0;
+        fgets(book[i].title,29,fp);
+        fgets(book[i].auth,29,fp);
+        fgets(book[i].publ,29,fp);
+        //문자열의 맨 마지막 NULL 문자 이전에 개행문자 '\n' 을 NULL 문자로 바꾸기
+        for(int j=0;j<30&&condition!=3;j++){
+            if(book[i].title[j]=='\n'){book[i].title[j]=NULL; condition++;}
+            if(book[i].auth[j]=='\n'){book[i].auth[j]=NULL;condition++;}
+            if(book[i].publ[j]=='\n'){book[i].publ[j]=NULL;condition++;}
+        }
+        if(fgetc(fp)=='Y') book[i].borrowed = Y;
+        else book[i].borrowed = N;
+        fseek(fp,-1,SEEK_CUR);
+        
+        //파일 위치 지정자를 다음줄로 이동시키기위해 사용한 코드
+        fgets(buf,29,fp);
+    }
+    printf("Successfully loaded the file.\n\n");
+    fclose(fp);
+    return 0;
+}
+
+//문자열을 정수로 변환시키는 함수
+int get_int(char *num){
+     int digit=1, integer=0;
+
+    for(int i=1;num[i]&&num[i]!='\n';i++){
+        digit*=10;
+    }
+    for(int index=0;num[index]&&num[index]!='\n';index++){
+        integer+=digit*(num[index]-48);
+        digit/=10;
+    }
+    return integer;
 }

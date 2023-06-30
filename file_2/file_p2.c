483253ca020ff39a8a21a6f4e740529a693c31e9
saegeulu <saegeulu@naver.com>
2023-06-30 21:17:37 +0900

    파일 문제 2 -ing

---
+++ file_2/file_p2.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int print_line_number(char word[]);
+
+int main(){
+    char word[30];
+    printf("Search: ");
+
+    scanf("%s",word);
+
+    print_line_number(word);
+
+    return 0;
+}
+
+int print_line_number(char word[]){
+    char c;
+    FILE*fp = fopen("C:\\Users\\dnjsg\\source\\C_language_training\\file_2\\p2.txt","r");
+    if(fp==NULL){
+        printf("READ ERROR!!!\n");
+        return 0;
+    }
+    
+    int line_num=1;
+    while((c=fgetc(fp))!=EOF){
+        int i=0;
+        if(word[i]==c){
+            fseek(fp, -1, SEEK_CUR);
+            while(word[i]==fgetc(fp)){
+                if(word[i+1]==NULL) printf("The line number: %d\n",line_num); 
+                i++;
+            }
+            fseek(fp,-i,SEEK_CUR);
+        }
+        if(c=='\n') line_num++;
+    }
+    
+    fclose(fp);
+    return 0;
+}

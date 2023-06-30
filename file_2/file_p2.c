255d38f4174169decab58c5e87f2d5375b65ac15
saegeulu <saegeulu@naver.com>
2023-06-30 21:20:16 +0900

    파일 문제 2 해결

--- file_2/file_p2.c
+++ file_2/file_p2.c
@@ -14,27 +14,31 @@ int main(){
 }
 
 int print_line_number(char word[]){
+    int check=0;
     char c;
     FILE*fp = fopen("C:\\Users\\dnjsg\\source\\C_language_training\\file_2\\p2.txt","r");
     if(fp==NULL){
         printf("READ ERROR!!!\n");
         return 0;
     }
-    
+
     int line_num=1;
     while((c=fgetc(fp))!=EOF){
         int i=0;
         if(word[i]==c){
             fseek(fp, -1, SEEK_CUR);
             while(word[i]==fgetc(fp)){
-                if(word[i+1]==NULL) printf("The line number: %d\n",line_num); 
+                if(word[i+1]==NULL) {
+                    printf("The line number: %d\n",line_num); 
+                    check=1; 
+                }
                 i++;
             }
             fseek(fp,-i,SEEK_CUR);
         }
         if(c=='\n') line_num++;
     }
-    
+    if(check==0)    printf("Not exist\n");
     fclose(fp);
     return 0;
 }
